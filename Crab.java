07a99fb88790cabfb3f1d8ed1ca238dfb1db3bd8
Eloy Hernández <eloyhz@gmail.com>
2018-09-27 10:46:11 -0500

    Cangrejo que se mueve con las flechas izquierda y derecha.

---
+++ Crab.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Crab here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Crab extends Actor
+{
+    /**
+     * Act - do whatever the Crab wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        super.move(4);
+        if (Greenfoot.isKeyDown("right"))   {
+            turn(3);
+        }
+        if (Greenfoot.isKeyDown("left"))    {
+            turn(-3);
+        }
+    }    
+}
+
+
+
+
+
+
+
+
